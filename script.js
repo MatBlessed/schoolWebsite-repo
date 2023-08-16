cabf44e4c13a99839d9c8040b9df090978c89763
MatBlessed <mathewblessed.002@gmail.com>
2023-08-16 11:14:59 +0530

    Styling .navbar with script

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("navbar").style.height = "50px";
+    document.getElementById("navbar").style.borderRadius = "0px 0px 5px 5px";
+    document.getElementById("logo").style.width = "4%";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("navbar").style.height = "100px";
+    document.getElementById("navbar").style.borderRadius = "0px";
+    document.getElementById("logo").style.width = "8%";
   }
 }
